2216c815abf6a9ab383082b9ac96d112ecb99891
Mikael <mikaelfontoura29@gmail.com>
2025-05-14 21:41:38 -0300

    Código base e compreensão do trabalho

---
+++ Estrutura de Dados/trabalho_estruturas/simulacao.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "tad_configs.h"
+
+void simular(int tempo) {
+    printf("Simulando...\n");
+    sleep(tempo);
+}
+
+int main() {
+    TadConfigs *tad_configs;
+    // Criar TAD e abrir arquivo
+    tad_configs = configs_inicializar();
+    if (!tad_configs) {
+      printf("Erro ao criar TAD\n");
+      return 1;
+    }
+    printf("Arquivo acessado!");
+    // Carregar configurações
+    configs_ler(tad_configs);
+    configs_mostrar(tad_configs);
+    while(tad_configs->configs.status != TERMINAR) {
+        sleep(1);
+        if(tad_configs->configs.status == SIMULAR) {
+            simular(2);
+        } else {
+            printf("Aguardando...\n");
+        }
+        configs_ler(tad_configs);
+    }
+
+    return 0;
+}
+/*Deve-se implementar uma fila utilizando lista encadeada.
+No menu, o usuário cria uma nova ficha, um tempo (de preferência
+aleatório) é associado a esta ficha. Depois, adicionar a mesma na fila.
+Na TV, conforme o tempo passa, os itens são removidos da fila e
+apresentados na tela. Quando terminar o tempo, chama o próximo.*/
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Disciplinas-Faculdade
+# Disciplinas da Faculdade
+---
 Repositório voltado para o gerenciamento e armazenamento de códigos desenvolvidos em diversas disciplinas ao longo da faculdade de Engenharia de Computação.
